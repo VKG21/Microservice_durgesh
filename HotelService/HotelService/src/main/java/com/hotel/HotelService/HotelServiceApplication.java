a3f18ed1d238fa8d9897eac2cce35655730ca2b4
VKG21 <Vishalgavale143@gmail.com>
2023-05-27 11:02:32 +0530

    first commit

--- HotelService/HotelService/src/main/java/com/hotel/HotelService/HotelServiceApplication.java
+++ HotelService/HotelService/src/main/java/com/hotel/HotelService/HotelServiceApplication.java
@@ -10,6 +10,8 @@ public class HotelServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelServiceApplication.class, args);
+		System.out.println("hhi");
 	}
 
+	
 }
